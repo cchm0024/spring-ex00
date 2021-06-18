ca1534b36d3a3acc72c8bbf9c0bfc8e6e442a01e
cchm0024 <cchm0024@naver.com>
2021-06-18 15:26:32 +0900

    20210618

---
+++ src/test/java/org/zerock/controller/lecture/Ex07Controller.java
@@ -0,0 +1,54 @@
+package org.zerock.controller.lecture;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.zerock.controller.lecture.domain.User;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/ex07/*")
+@Log4j
+public class Ex07Controller {
+	
+	@RequestMapping("/sub01")
+	public @ResponseBody String method01() {
+		log.info("ex07, sub01 method");
+		
+		return "my responer!";
+	}
+	
+	@RequestMapping("/sub02")
+	public @ResponseBody String method02() {
+		log.info("ex07, sub02 method");
+		
+		User user = new User();
+		user.setName("edward");
+		user.setAge(99);
+		
+		return user.toString();
+	}
+	
+	@RequestMapping("/sub03")
+	public @ResponseBody String method03() {
+		log.info("ex07, sub03 method");
+		
+		User user = new User();
+		user.setName("donald");
+		user.setAge(99);
+		
+		return "{\"id\":" + "\"" + user.getName() + "\", " + "\"age\":" + user.getAge() + "}";
+	}
+	
+	@RequestMapping("/sub04")
+	public @ResponseBody User method04() {
+		log.info("ex07, sub04 method");
+		
+		User user = new User();
+		user.setName("edward");
+		user.setAge(99);
+	
+		return user;
+}
+}
