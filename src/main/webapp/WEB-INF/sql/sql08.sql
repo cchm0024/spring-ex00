af63a6fe2ba26b210bf56145fbf39ed2274172cb
cchm0024 <cchm0024@naver.com>
2021-07-06 18:16:58 +0900

    20210706

---
+++ src/main/webapp/WEB-INF/sql/sql08.sql
@@ -0,0 +1,26 @@
+USE spr1;
+
+DROP table tbl_member_auth;
+DROP table tbl_member;
+
+CREATE TABLE tbl_member(
+	userid VARCHAR(50) PRIMARY KEY,
+    userpw VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    regdate TIMESTAMP DEFAULT NOW(),
+    updateDate TIMESTAMP DEFAULT NOW(),
+    enabled TINYINT(1) DEFAULT 1
+);
+
+CREATE TABLE tbl_member_auth (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    userid VARCHAR(50) NOT NULL,
+    auth VARCHAR(50) NOT NULL,
+    FOREIGN KEY (userid) REFERENCES tbl_member(userid)
+
+);
+
+SELECT * FROM tbl_member;
+SELECT * FROM tbl_member_auth;
+
+
